903d705a425c4bcc9a08e2d9ade6ad54e85d8a2c
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2020-04-13 22:24:20 +0300

    + Created manual exception description

---
+++ LAB4/src/exceptions/HumanNotFromVehicle.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class HumanNotFromVehicle extends Exception {
+    HumanNotFromVehicle(String msg) {
+        super(msg);
+    }
+}
---
+++ LAB4/src/exceptions/VehicleFull.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class VehicleFull extends Exception {
+    VehicleFull(String msg) {
+        super(msg);
+    }
+}
