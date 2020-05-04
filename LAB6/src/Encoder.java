a5f255ab2965e6cb4097850ddf4d0b16e375c25c
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2020-05-05 01:31:40 +0300

    + Added inherited method to decoder and encoder

--- LAB6/src/Encoder.java
+++ LAB6/src/Encoder.java
@@ -1,8 +1,16 @@
 import java.io.FilterWriter;
+import java.io.IOException;
 import java.io.Writer;
 
 public class Encoder extends FilterWriter {
     public Encoder (Writer writer) {
         super(writer);
+    }
+
+    public void write(String str, char keySymbol) throws IOException {
+        char[] strByChar = str.toCharArray();
+        for (char c : strByChar)
+            super.write((int) c + (int) keySymbol);
+        super.write('\n');
     }
 }
